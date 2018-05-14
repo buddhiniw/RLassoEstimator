9b9babfb3a645c2c5c9f8d9e1df6f35540f69dcc
buddhiniw <buddhiniw@gmail.com>
2018-05-14 12:05:59 -0500

    Implemented OLS analysis with standardized results.

--- BEDROCK-LASSO.R
+++ BEDROCK-LASSO.R
@@ -20,6 +20,7 @@
 #############################
 library(glmnet) # Package to fit ridge/lasso/elastic net models
 library(selectiveInference)
+library(DMwR)
 
 rm(list=ls())
 
@@ -28,7 +29,7 @@ rm(list=ls())
 ################ BEGIN: THIS SECTION MUST BE CUSTOMIZED FOR SYSTEM ################
 ##  Set the working directory
 home =Sys.getenv("HOME")
-wd = paste0(home,"/MyWork/Upwork/R_bedrock_gui")
+wd = paste0(home,"/MyWork/Upwork/R_bedrock_gui/RLassoEstimator")
 setwd(wd)
 
 # get file name from command line arguments if run as script from command line or PHP
@@ -53,57 +54,90 @@ inFileName=args[1]
 #dependent variables.#
 dataIn <- read.csv(inFileName, header=T)
 
-#standarize the predictor variables prior to fitting the model using Lasso regularization
-# Then the penalization will treat different scale explanatory variables on a more equal footing
-x.scaled <- as.data.frame(scale(dataIn[,-1],center=TRUE, scale=TRUE))
 
+# First standardize the predictor variables
+#
+# Standarizing/scaling the predictor variables prior to fitting the model will ensure
+# the lasso penalization will treat different scale explanatory variables on a more equal footing
+# But only continuos predictors needs to be standardized.
+# DONOT scale factor variables (using 1/0) used for yes/no situations.
+# Therefore seperate the dataframe with continuos variables and factors
+
+# Get x variables (including the test data row)
+dataIn.x <- dataIn[,-1]
+
+# Identify columns with categorical data (0/1)
+cat.vars <- apply(dataIn.x,2,function(x) { all(x %in% 0:1) })
+dataIn.continuous <- dataIn.x[!cat.vars]
+dataIn.catrgorical <- dataIn.x[cat.vars]
+
+# Now scale the continuous varibales ONLY
+dataIn.continuous.scaled <-scale(dataIn.continuous,center=TRUE, scale=TRUE)
+
+# Create the scaled x matrix
+x.scaled <- cbind(as.data.frame(dataIn.continuous.scaled),dataIn.catrgorical)
+
+
+dataIn.y <- dataIn[1:(nrow(dataIn)-1),1,drop=FALSE]
+dataIn.y.scaled <- scale(dataIn.y,center=TRUE, scale=TRUE)
+y.scaled <- as.data.frame(dataIn.y.scaled)
 
 # Create the X and Y regression matrices
 x <- as.matrix(x.scaled[1:(nrow(x.scaled)-1),])
-y <- as.matrix(dataIn[1:(nrow(dataIn)-1),1])
+y <- as.matrix(y.scaled)
 
-#Get the test data set assuming the last row  in the original data set is the test set
+
+# Get the test data set assuming the last row  in the original data set is the test set
 dataIn.scaled.test <- x.scaled[nrow(x.scaled),]
 x.test <-as.matrix(dataIn.scaled.test)
 
 
 # Fit LASSO using glmnet(y=, x=). Gaussian model is default. alpha=1 for lasso only.
 # Set standardize = FALSE because the variables were standardized in the begining of the 
-# analysis using "scale" function.
+# analysis using "scale" function. intercept =FALSE because responce variables(y) is also scaled
 # Function produces series of fits for many values of lambda. 
-fit.glmnet.lasso <- glmnet(x,y,family="gaussian",alpha=1,standardize=F)
+fit.glmnet.lasso <- glmnet(x,y,family="gaussian",alpha=1,standardize=FALSE,intercept = FALSE)
 plot(fit.glmnet.lasso, xvar="lambda")
 
 # Perform nfolds=sample size cross-validation  (leave-one-out-cross-validation)
 # runs glmnet nfolds+1 times; the first to get the lambda sequence, and then the
 # remainder to compute the fit with each of the folds omitted. 
 # The error is accumulated, and the average error and standard deviation over the folds is computed
-fit.glmnet.lasso.cv=cv.glmnet(x,y,alpha=1,nfolds = length(y),type.measure="mse") 
+fit.glmnet.lasso.cv <- cv.glmnet(x,y,alpha=1,nfolds = length(y),type.measure="mse") 
 plot(fit.glmnet.lasso.cv)
-lasso.glmnet = fit.glmnet.lasso.cv$glmnet.fit
+lasso.glmnet <- fit.glmnet.lasso.cv$glmnet.fit
 
 # the λ at which the minimal MSE is achieved
-lambda.min = fit.glmnet.lasso.cv$lambda.min
+lambda.min <- fit.glmnet.lasso.cv$lambda.min
 #print(lambda.min)
 
 # make predictions from the cross-validated glmnet model using lambda min(optimal lambda)
-y.hat.glmnet = predict(fit.glmnet.lasso.cv,x.test,s="lambda.min")
+y.hat.glmnet.scaled <- predict(fit.glmnet.lasso.cv,x.test,s="lambda.min")
+# unscale to get the actual magnitude
+y.hat.glmnet.unscaled <- unscale(y.hat.glmnet.scaled,dataIn.y.scaled)
+print(y.hat.glmnet.unscaled)
+
 # get the model coefficients at optimal lambda (lambda min)
-beta.hat.glmnet = coef(fit.glmnet.lasso.cv,s="lambda.min")
+beta.hat.glmnet.scaled <- coef(fit.glmnet.lasso.cv,s="lambda.min")
 
 
-# In the following section, we are going to perform simple linear regression
+# In the following section, we are going to perform ordinary least squares (OLS) regression
 # on the data set to cross-compare the model coefficients with the lasso results.
 
-# The formula used for simple linear regression model 
+# Get the formula used for simple linear regression model 
 y.name=names(dataIn)[1]
 x.names=names(dataIn)[-1]
 formula.lm = paste(y.name,paste0(x.names,collapse=' + '),sep=' ~ ')
-dataIn.train <- dataIn[1:(nrow(dataIn)-1),]
+#koala.min.formula=paste(wombat.y.name,paste0(wombat.x.names[koala.min.Xs],collapse=' + '),sep=' ~ ')
+
+# Since the Lasso analysis was done with standardized data, we are going to use
+# the standardized data as a training set for the OLS too. 
+dataIn.train <- cbind(y.scaled,x.scaled[1:(nrow(x.scaled)-1),])
 dataIn.test <-dataIn[nrow(dataIn),-1]
+
+# Make predictions from the simple linear model for comparision
 fit.lm <- lm(formula.lm, data=dataIn.train)
-# Make predictions from the simple linear model
 y.hat.lm <- predict(fit.lm,newdata=dataIn.test,interval="prediction")
 
-
-# source("BEDROCK-DOC.R")
+source("BEDROCK-HELP-FUNC.R")
+source("BEDROCK-DOC.R")
